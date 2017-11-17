ce256192a20925ad1de172eccc7424ad8c0bc612
Daybson Paisante <daybson.paisante@outlook.com>
2017-11-17 12:25:34 -0200

    * MicroEngine.csproj -> .NET Standard 2.0

--- SourceCode/MicroEngine/src/Core/Game.cs
+++ SourceCode/MicroEngine/src/Core/Game.cs
@@ -15,11 +15,7 @@ namespace MicroEngine.Core
         public List<Entity> Entities = new List<Entity>();
 
         public Game()
-        {
-            //Entities.Add(new Entity(new Circle(20, new Vector2f())));
-            //Entities.Add(new Entity(new Rectangle(20, 50, new Vector2f())));
-            Entities.Add(new Entity(new SegmentLine(new Vector2f(), new Vector2f(50,50))));
-            //Entities.Add(new Entity(@"D:\Repositorio\MicroEngine\SourceCode\MicroEngine\res\textures\tri.png"));
+        {            
             IsRuning = true;
         }
 
